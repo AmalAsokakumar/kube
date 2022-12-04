b0e3e1312c3b1e1f5ecb003d35ab58f229a314f1
comrider <cena3799@gmail.com>
2022-12-04 18:39:13 +0530

    file modified

---
+++ uninstall.sh
@@ -0,0 +1,19 @@
+NAME                                     READY   STATUS             RESTARTS        AGE
+adservice-56744b788d-gldd6               0/1     ImagePullBackOff   0               23m
+adservice-56744b788d-lm224               0/1     ImagePullBackOff   0               23m
+cartservice-687f7fdb9d-k9gp7             0/1     CrashLoopBackOff   9 (2m13s ago)   23m
+cartservice-687f7fdb9d-xh6vj             0/1     CrashLoopBackOff   9 (97s ago)     23m
+checkoutservice-8dcc57645-6b9dl          1/1     Running            0               23m
+checkoutservice-8dcc57645-rw5x8          1/1     Running            0               23m
+currencyservice-77fb84ccd6-gjltp         1/1     Running            0               23m
+currencyservice-77fb84ccd6-kbggc         1/1     Running            0               23m
+emailservice-666d46c77d-k5jzg            1/1     Running            0               23m
+emailservice-666d46c77d-k5xhs            1/1     Running            0               23m
+paymentservice-b9fdb49bc-gn22j           1/1     Running            0               23m
+paymentservice-b9fdb49bc-hbn99           1/1     Running            0               23m
+productcatalogservice-55cb58c47f-89jkm   1/1     Running            0               23m
+productcatalogservice-55cb58c47f-zwhts   1/1     Running            0               23m
+recommendationservice-6d86c7f9d-tm79v    1/1     Running            0               23m
+recommendationservice-6d86c7f9d-x9jjh    1/1     Running            0               23m
+shippingservice-56ccfb7bd9-tcxxw         1/1     Running            0               23m
+shippingservice-56ccfb7bd9-tlxdx         1/1     Running            0               23m
